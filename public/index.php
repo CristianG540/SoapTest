beedbeec4b5a2fb1f025100fb048727dcc7d58ee
CristianG540 <cristianneatproy@gmail.com>
2017-06-01 05:54:03 -0500

    Logro hacer la peticion al servicio de login

--- public/index.php
+++ public/index.php
@@ -23,6 +23,8 @@ require __DIR__ . '/../src/dependencies.php';
 // Register middleware
 require __DIR__ . '/../src/middleware.php';
 
+require __DIR__ . '/../src/IgbSoapLogin.php';
+
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,44 @@
 // Routes
 
 $app->get('/[{name}]', function ($request, $response, $args) {
+    xdebug_break();
+    $login = new nusoap_client("http://b1ws.igbcolombia.com/B1WS/WebReferences/LoginService.wsdl", true);
+    $error  = $login->getError();
+    $params = array(
+        'DatabaseServer'  => '192.168.10.102', //string
+        'DatabaseName'    => 'MERCHANDISING', //string
+        'DatabaseType'    => 'dst_MSSQL2012', //DatabaseType
+        'CompanyUsername' => 'manager', //string
+        'CompanyPassword' => 'Pa$$w0rd', //string
+        'Language'        => 'ln_Spanish', //Language
+        'LicenseServer'   => '192.168.10.102:30000' //string
+    );
+    $soapRes = $login->call('Login', $params);
+
+    /*$client = new SoapClient('http://b1ws.igbcolombia.com/B1WS/WebReferences/LoginService.wsdl');*/
+    //$client->__setLocation('http://b1ws.igbcolombia.com/B1WS/Service.asmx');
+    /*$result = $client->__soapCall("LoginService/Login", array(
+        'DatabaseServer'  => '192.168.10.102', //string
+        'DatabaseName'    => 'MERCHANDISING', //string
+        'DatabaseType'    => 'dst_MSSQL2012', //DatabaseType
+        'CompanyUsername' => 'manager', //string
+        'CompanyPassword' => 'Pa$$w0rd', //string
+        'Language'        => 'ln_Spanish', //Language
+        'LicenseServer'   => '192.168.10.102:30000' //string
+    )); */
+    /*$res = $client->Login(array(
+        'DatabaseServer'  => '192.168.10.102', //string
+        'DatabaseName'    => 'MERCHANDISING', //string
+        'DatabaseType'    => 'dst_MSSQL2012', //DatabaseType
+        'CompanyUsername' => 'manager', //string
+        'CompanyPassword' => 'Pa$$w0rd', //string
+        'Language'        => 'ln_Spanish', //Language
+        'LicenseServer'   => '192.168.10.102:30000' //string
+    ));*/
+
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.phtml', ['data' => $soapRes]);
 });
